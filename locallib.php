f963bfdc2f942e60481fcd2f5863cf480c81e649
Sangyul Cha <scha-ms@ssystems.de>
2026-09-10 14:55:21 +0200

    TEST-B code-change: full CI must succeed (20260910T1250Z).

--- locallib.php
+++ locallib.php
@@ -200,3 +200,5 @@ function resort_course_category($category, $cronrunning = false) {
     // If we have arrived here, the category should be sorted.
     return true;
 }
+
+// TEST-B code-change marker 20260910T1250Z
